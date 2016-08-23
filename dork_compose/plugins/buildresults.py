91a822749795fedb3753dd256c1eee00a98eb0c3
Philipp Melab <philipp.melab@gmail.com>
2016-08-23 17:51:37 +0200

    Fixed buildresult syncing edge cases.

--- dork_compose/plugins/buildresults.py
+++ dork_compose/plugins/buildresults.py
@@ -2,6 +2,7 @@ import dork_compose.plugin
 import tempfile
 from tarfile import TarFile
 import os
+import shutil
 
 
 class Plugin(dork_compose.plugin.Plugin):
@@ -12,15 +13,18 @@ class Plugin(dork_compose.plugin.Plugin):
         for container in containers:
             if 'dork.buildresults.paths' in container.labels and 'dork.buildresults.root' in container.labels:
                 root = container.labels['dork.buildresults.root']
-                paths = container.labels['dork.buildresults.paths'].split(';')
+                paths = filter(lambda x: x, container.labels['dork.buildresults.paths'].split(';'))
                 for path in paths:
+                    print "Synching %s." % path
                     tmp = tempfile.NamedTemporaryFile(delete=False)
+                    container.client.exec_start(container.client.exec_create(container.id, "mkdir -p %s/%s" % (root, path)))
                     response, info = container.client.get_archive(container.id, "%s/%s" % (root, path))
                     for chunk in response.stream(1024):
                         tmp.write(chunk)
                     tmp.close()
                     tar = TarFile.open(tmp.name)
+                    if os.path.isdir("%s/%s" % (source, path)):
+                        shutil.rmtree("%s/%s" % (source, path))
                     tar.extractall(os.path.dirname("%s/%s" % (source, path)))
-                    os.remove(tmp.name)
 
 
