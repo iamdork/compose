50a4849ea69c5692baf5cbee21087f8792369aa5
Philipp Melab <philipp.melab@gmail.com>
2016-09-16 09:36:44 +0200

    Use TRAVIS_TAG as version.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
+import os
+
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
@@ -14,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.8.0',
+    version=os.environ.get('TRAVIS_TAG'),
 
     description='docker-compose with magicks.',
 
