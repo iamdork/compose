d1096b13f6e37ca3786278c706221e653811ddee
Campbell Vertesi <campbell@vertesi.com>
2017-01-02 11:01:38 +0100

    Add a helper function is_subdir, use it to check directory relation in autobuild plugin

--- dork_compose/helpers.py
+++ dork_compose/helpers.py
@@ -1,6 +1,13 @@
+import os;
+
 def tru(val):
     return val
 
-
 def notdefault(val):
     return val if val != 'default' else None
+
+def is_subdir(path, directory):
+    path = os.path.realpath(path)
+    directory = os.path.realpath(directory)
+    relative = os.path.relpath(path, directory)
+    return not (relative == os.pardir or relative.startswith(os.pardir + os.sep))
