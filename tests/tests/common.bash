a0569a1297f10847f22f6aaf8a2066a87a00028a
Philipp Melab <philipp.melab@gmail.com>
2017-02-03 12:07:00 +0100

    #29: Removed dependency plugin from tests.

--- tests/tests/common.bash
+++ tests/tests/common.bash
@@ -3,7 +3,7 @@
 export SOURCES="$(pwd)/sources"
 export DORK_LIBRARY_PATH="$(pwd)/lib"
 export DORK_PROXY_HTTPS_SIGNING="none"
-export DORK_PLUGINS='env:multi:lib:autobuild:hotcode:dependencies:git:filesystem:proxy:dns:vault'
+export DORK_PLUGINS='env:multi:lib:autobuild:hotcode:git:filesystem:proxy:dns:vault'
 
 get() {
   curl --resolve $1:80:127.0.0.1 http://$1/$2
